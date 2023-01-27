c726bcebb1f1b171332afa4aac4edce5690bbb57
Victoria <victoria.constantini@etu.minesparis.psl.eu>
2023-01-27 12:03:08 +0100

    Vic la best

--- rogue.py
+++ rogue.py
@@ -2,6 +2,63 @@
 
 # imports:
 import pygame as pg
+import random as rd 
+
+
+class Game : 
+
+    def get_arrow(self, heros):
+
+        for event in pg.event.get():
+        #fermer la fenêtre
+            if event.type == pg.QUIT:
+                self.running = False
+            elif event.type == pg.KEYDOWN:
+                # si la touche est "Q" on veut quitter le programme
+                if event.key == pg.K_q:
+                    self.running = False
+                #faire le lien entre flèche du clavier et direction du serpent
+                elif event.key == pg.K_DOWN:
+                    heros.direction = [0,1]
+                elif event.key == pg.K_UP:
+                    heros.direction = [0,-1]
+                elif event.key == pg.K_RIGHT:
+                    heros.direction = [1,0]
+                elif event.key == pg.K_LEFT:
+                    heros.direction = [-1,0]
+                elif event.key == pg.K_p:
+                    heros.boire(heros.potion) 
+
+
+#classe       
+def generer_ennemi(chambre, matrice):
+    (m,n,o,p) = get_position_coin(chambre)
+    i = rd.randint(m,n)
+    j = rd.randint(o,p)
+    matrice[i,j] = 9
+
+def ennemis_alentours(heros, ennemi):
+    pos_ennemi = ennemi.position
+    pos_heros = heros.position
+    if abs(pos_ennemi[0]-pos_heros[0]) ==  1 : 
+        return True
+    elif abs(pos_ennemi[1]-pos_heros[1]) ==  1 : 
+        return True
+    else : 
+        return False
+
+def ennemi_suit_joueur(heros,ennemi):
+    pos_joueur = heros.position
+    pos_ennemi = ennemi.position 
+    while not ennemis_alentours : #et joueur encore dans la pièce
+        if pos_joueur[0] > pos_ennemi[0] :
+            ennemi.mouvement(np.array([1,0]))
+        elif pos_joueur[0] < pos_ennemi[0] :
+            ennemi.mouvement(np.array([-1,0]))
+        elif pos_joueur[1] < pos_ennemi[1] :
+            ennemi.mouvement(np.array([0,-1]))
+        else : 
+            ennemi.mouvement(np.array([0,1]))     
 
 
 def main():
@@ -18,3 +75,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
